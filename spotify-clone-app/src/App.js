763ac911bb12ad7229d890a805f2db160d3994d8
Robert <r.powelliv@gmail.com>
2020-11-13 17:11:40 -0500

    devlop

--- spotify-clone-app/src/App.js
+++ spotify-clone-app/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
+import Login from "./Login";
+import { getTokenFromUrl } from './spotify';
 
 function App() {
+  
+  // Runs code based on a given condition
+useEffect(() => {
+  const token = getTokenFromUrl();
+  console.log('TOKEN IS >>>', token)
+  window.location.hash = "";
+}, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Login />
+
     </div>
   );
 }
